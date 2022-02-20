48b3d13a2063ad409be385014212dec7b8e59b72
Viola <viola.cailiu@gmail.com>
2022-02-20 20:29:38 +0100

    homepage

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomepageComponent } from './homepage/homepage.component';
 import { InformationComponent } from './information/information.component';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
@@ -19,6 +21,7 @@ import { MatCardModule } from '@angular/material/card';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     MatTableModule,
     MatIconModule,
     MatButtonModule,
