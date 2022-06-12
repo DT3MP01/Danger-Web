5baa3b148e7d005d479b1d381ea9953af4551f92
DT3MP01 <albertoalvaza@gmail.com>
2022-06-12 03:59:54 +0200

    Pagination 1

--- src/app/components/scoreboard/scoreboard.component.ts
+++ src/app/components/scoreboard/scoreboard.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵsetAllowDuplicateNgModuleIdsForTest } from '@angular/core';
 
 import { game } from '../../shared/game';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { ScoreService } from 'src/app/services/score.service';
+import { PageEvent } from '@angular/material/paginator';
 
 
 
@@ -13,9 +14,12 @@ import { ScoreService } from 'src/app/services/score.service';
 })
 
 export class ScoreboardComponent implements OnInit {
+  
 
   dataSource = new MatTableDataSource<game>();
   displayedColumns: string[] = ['username', 'scans', 'time', 'meters'];
+
+
 
   constructor(private ScoreService: ScoreService) { }
 
@@ -27,4 +31,19 @@ ngAfterContentInit() {
       games => this.dataSource.data = games
     );
   }
+
+    // MatPaginator Inputs
+    length = 85;
+    pageSize = 10;
+    pageSizeOptions: number[] = [10, 20, 30, 50];
+
+    AllGames:  Array<number>=Array.from({length: this.length}, (_, i) => i + 1);
+    numberOfGames: number[]=this.AllGames.slice(0, this.pageSize);
+  
+    // MatPaginator Output
+    pageEvent: PageEvent= new PageEvent();
+  
+    setPageSizeOptions() {
+        this.numberOfGames=this.AllGames.slice(this.pageEvent.pageIndex*this.pageSize, this.pageEvent.pageIndex*this.pageSize+this.pageSize);
+    }
 }
